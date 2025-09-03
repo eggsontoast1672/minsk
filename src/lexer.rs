c04e1f53eda210195d12dc4545d66c2b478f3009
Paul Zupan <paul.zupan.13@gmail.com>
2025-09-03 12:53:17 -0700

    Complete lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,3 +1,4 @@
+#[derive(Clone, Debug)]
 pub enum Token {
     Plus,
     Minus,
@@ -7,4 +8,86 @@ pub enum Token {
     ParenRight,
     Number(i32),
     EndOfFile,
+}
+
+#[derive(Debug)]
+pub enum LexError {
+    InvalidToken(char),
+    InvalidInteger(std::num::ParseIntError),
+}
+
+impl std::fmt::Display for LexError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::InvalidToken(c) => write!(f, "invalid token '{c}'"),
+            Self::InvalidInteger(e) => write!(f, "{e}"),
+        }
+    }
+}
+
+impl std::error::Error for LexError {}
+
+pub struct Lexer<I: Iterator> {
+    chars: std::iter::Peekable<I>,
+    dispensed_eof: bool,
+}
+
+impl<'a> Lexer<std::str::Chars<'a>> {
+    pub fn from_source(source: &'a str) -> Self {
+        Self {
+            chars: source.chars().peekable(),
+            dispensed_eof: false,
+        }
+    }
+}
+
+impl<I: Iterator<Item = char>> Lexer<I> {
+    pub fn skip_whitespace(&mut self) {
+        while let Some(c) = self.chars.peek()
+            && c.is_whitespace()
+        {
+            self.chars.next();
+        }
+    }
+
+    fn lex_number(&mut self, first: char) -> <Self as Iterator>::Item {
+        let mut digits = first.to_string();
+        while let Some(d) = self.chars.peek()
+            && d.is_ascii_digit()
+        {
+            digits.push(*d);
+            self.chars.next();
+        }
+
+        return match digits.parse::<i32>() {
+            Ok(value) => Ok(Token::Number(value)),
+            Err(error) => Err(LexError::InvalidInteger(error)),
+        };
+    }
+}
+
+impl<I: Iterator<Item = char>> Iterator for Lexer<I> {
+    type Item = Result<Token, LexError>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.skip_whitespace();
+
+        match self.chars.next() {
+            Some('+') => Some(Ok(Token::Plus)),
+            Some('-') => Some(Ok(Token::Minus)),
+            Some('*') => Some(Ok(Token::Star)),
+            Some('/') => Some(Ok(Token::Slash)),
+            Some('(') => Some(Ok(Token::ParenLeft)),
+            Some(')') => Some(Ok(Token::ParenRight)),
+            Some(other) if other.is_ascii_digit() => Some(self.lex_number(other)),
+            Some(other) => Some(Err(LexError::InvalidToken(other))),
+
+            // We only want to emit the EndOfFile token once per stream, right at the end.
+            None if self.dispensed_eof => None,
+            None => {
+                self.dispensed_eof = true;
+                Some(Ok(Token::EndOfFile))
+            }
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,24 @@
-use minsk::{ast, eval, lexer::Token, parser::Parser};
+use minsk::{ast, eval, lexer::Lexer, parser::Parser};
 
-fn main() {
-    let tokens = vec![
-        Token::Number(1),
-        Token::Star,
-        Token::Number(2),
-        Token::Plus,
-        Token::Number(3),
-        Token::EndOfFile,
-    ];
+fn the_whole_kitchen_sink(source: &str) {
+    println!("Source: {source}");
 
-    let mut parser = Parser::new(tokens);
-    let ast = parser.parse();
+    let tokens: Vec<_> = Lexer::from_source(source).map(|t| t.unwrap()).collect();
+    println!("\nTokens:\n[");
+    for token in &tokens {
+        println!("    {token:?},");
+    }
+    println!("]");
 
+    let ast = Parser::new(tokens).parse();
+    println!("\nAST:");
     ast::pretty_print(&ast);
 
     let result = eval::evaluate(ast);
+    println!("\nResult: {result}");
+}
 
-    println!("result = {result}");
+fn main() {
+    let source = "1 + 2 * 3";
+    the_whole_kitchen_sink(source);
 }
